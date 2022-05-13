6459685b34e2d15ca70213bd2a886dc35b8f7f76
Mario de León <46909802+DavidDLM@users.noreply.github.com>
2022-05-12 21:13:45 -0600

    Update Nav.js

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -6,11 +6,9 @@ export default class Nav extends Component {
     render() {
         return (
             <div className="nav">
-                <div className="nav__blocks">
+                <div className="nav-blocks">
                     <img src={logo} alt="chatlogo"></img>
                 </div>
-                <div className="nav__blocks"></div>
-                <div className="nav__blocks"></div>
             </div>
         )
     }
