e7777760046684d91a4e65d2e55b903b0da7676c
GitElean <eleanjulian@gmail.com>
2022-05-15 23:57:15 -0600

    contenedor del chat

--- ecommerce-frontend/src/components/ChatBubble.jsx
+++ ecommerce-frontend/src/components/ChatBubble.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState } from 'react';
+import '../styles/chat.css';
+import Chat from './svg/chat';
 
-function ChatBubble() {
-  return <div>ChatBubble</div>;
+const bubbleSta = {
+  open:{
+    bubble:'block'
+  },
+  closed:{
+    bubble:'hidden'
+  }
 }
 
-export default ChatBubble;
+const Chatbubble = () => {
+  const[bubbleState, setBubbleState] = useState('closed')
+
+  const initbubble = () =>{
+    if (bubbleState === 'closed'){
+      setBubbleState('open')
+    }
+    if (bubbleState === 'open'){
+      setBubbleState('closed')
+    }
+  }
+
+  return (
+    <div className="">
+      <div className="absolute right-2 bottom-2 ">
+        <div className="flex justify-end">
+          <div className={bubbleSta[bubbleState].bubble +  " bg-gradient-to-br from-primary2 to-primary1 w-[320px] h-[600px] rounded-md mx-auto p-8"}>
+            Componente chat
+          </div>
+          <div onClick={initbubble} className="bg-gradient-to-br from-primary2 to-primary1 w-8 h-8 rounded-full content-center ml-1.5 items-center object-center mx-auto hover:from-primary3 hover:to-primary2 duration-100 active:bg-primary4">
+            <div className="w-8 h-8 p-1"><Chat/></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Chatbubble;
+
