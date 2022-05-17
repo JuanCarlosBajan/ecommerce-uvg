b565a05e3602c8e71ac1fa850d82c8466edc59d4
Mario de León <46909802+DavidDLM@users.noreply.github.com>
2022-05-17 13:15:17 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './styles/chat.css';
+import Bubble from './componentes/ChatBubble'
+
+function App() {
+  return (
+    <div className="bg-bg2 h-screen w-screen">
+     <Bubble/>
+    </div>
+  );
+}
+
+export default App;
